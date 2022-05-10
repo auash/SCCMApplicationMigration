e6c8818d8a7ef0a21ef8999e159f01c48748584e
Asher Jebbink <asher.jebbink@hotmail.com>
2022-05-10 14:27:44 +1000

    Initial commit

---
+++ Export-SCCMApplications.ps1
@@ -0,0 +1,235 @@
+﻿$ErrorActionPreference = "Stop"
+
+
+#######################################################################
+#
+#   Step 1 - You MUST change these values to match your environment.
+#
+#######################################################################
+$SCCMSiteCode = "PR1"   # SCCM Site code you are exporting FROM
+$ProviderMachineName = "SCCM-01"   # Machine name of the SCCM server that has the SMS Provider service installed.
+
+
+#######################################################################
+#
+#   Step 2 - OPTIONAL: Change these if you desire.
+#
+#######################################################################
+$LogFilePath = "$($env:USERPROFILE)\Desktop\SCCMAppMigration-Export.log"    # Where this script will log to
+$AppsToProcessCsvPath = "$(Split-Path $MyInvocation.MyCommand.Path)\ToProcess.csv"    # The list of SCCM Applications that will be searched for and exported, as well as the export configuration.
+$RootAppInfoExportPath = "$(Split-Path $MyInvocation.MyCommand.Path)\AppInfo"       # Where Application exports will be saved to (meta data only, no content)
+$RootAppContentExportPath = "$(Split-Path $MyInvocation.MyCommand.Path)\Content"    # Where Application content will be copied to
+
+
+
+
+Function ApplicationHasDependencies($ApplicationName)
+{
+    $AppDeploymentInfo = $null
+    $AppDeploymentInfo = Get-CMDeploymentType -ApplicationName $ApplicationName
+
+    if ($AppDeploymentInfo.NumberOfDependedDTs -gt 0)
+    {
+        return $true
+    }
+    else
+    {
+        return $false
+    }
+}
+
+Function Display-AppsWithDependencies([string[]]$ApplicationNames)
+{
+    Write-Output ""
+    foreach ($Application in $ApplicationNames)
+    {
+        Write-Output "Warning: '$Application' has dependencies which will need to have their CONTENT fixed manually. The dependent apps will be created in the destination SCCM (when you run the Import script) but the content needs to be migrated and fixed manually."
+    }
+}
+
+Function AppCSVDataIsValid($AppCSV)
+{
+    if ([string]::IsNullOrWhiteSpace($AppCSV.SourceAppName) -or
+        [string]::IsNullOrWhiteSpace($AppCSV.DestinationAppName) -or
+        [string]::IsNullOrWhiteSpace($AppCSV.FolderManufacturer) -or
+        [string]::IsNullOrWhiteSpace($AppCSV.FolderAppName) -or
+        [string]::IsNullOrWhiteSpace($AppCSV.FolderAppVersion)
+    )
+    {
+        return $false
+    }
+    else
+    {
+        return $true
+    }
+}
+
+Function AppExistsInSCCM($ApplicationName)
+{
+    $App = $null
+    $App = Get-CMApplication -Name $ApplicationName -Fast
+
+    if ($App -eq $null)
+    {
+        return $false
+    }
+    else
+    {
+        return $true
+    }
+}
+
+Function AppHasSCCMContent($ApplicationName)
+{
+    return (Get-CMApplication -Name $ApplicationName -Fast).HasContent
+}
+
+Function ExportAppContent($ApplicationName, $DestinationPath)
+{
+    $App = $null
+    $App = Get-CMApplication -Name $ApplicationName
+    [xml]$Xml = $null
+    [xml]$Xml = $App.SDMPackageXML
+
+    if ($Xml.AppMgmtDigest.DeploymentType.Count -eq 0)
+    {
+        Write-Output "No Deployment Types found. No content to transfer."
+        return
+    }
+
+    foreach ($DT in $Xml.AppMgmtDigest.DeploymentType)
+    {
+        #Create a folder for each DT, with the DisplayName as the folder name
+        Write-Output "Creating the content folder for Deployment Type '$($DT.Title.'#text')'"
+        $DTContentPath = Join-Path -Path $DestinationPath -ChildPath $DT.Title.'#text'
+        New-Item -Path "FileSystem::$DTContentPath" -ItemType Directory -Force | Out-Null
+
+        #Copy files
+        Write-Output "Copying files from '$($DT.Installer.Contents.Content.Location)' to '$DTContentPath'"
+        Copy-Item -Path "FileSystem::$($DT.Installer.Contents.Content.Location)\*" -Destination "FileSystem::$DTContentPath" -Recurse -Force
+
+        Write-Output "Finished copying"
+    }
+
+}
+
+
+
+
+
+# Import the ConfigurationManager.psd1 module 
+if((Get-Module ConfigurationManager) -eq $null) {
+    Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1" 
+}
+
+# Connect to the site's drive if it is not already present
+if((Get-PSDrive -Name $SCCMSiteCode -PSProvider CMSite -ErrorAction SilentlyContinue) -eq $null) {
+    New-PSDrive -Name $SCCMSiteCode -PSProvider CMSite -Root $ProviderMachineName
+}
+
+# Set the current location to be the site code.
+Set-Location "$($SCCMSiteCode):\"
+
+
+Start-Transcript -Path $LogFilePath -Append
+
+cd "$($env:SystemDrive)\"
+Write-Output "Reading CSV for apps to process..."
+$CSVApps = Import-Csv -Path $AppsToProcessCsvPath
+cd "$($SCCMSiteCode):\"
+
+Write-Output "Read $($CSVApps.Count) apps from CSV file"
+
+$AppsThatHaveDependencies = @() #This gets output at the end of the script
+
+foreach ($CSVApp in $CSVApps)
+{
+    Write-Output ""
+
+    # Validate the CSV provided for this app is complete before continuing
+    if (-not (AppCSVDataIsValid -AppCSV $CSVApp))
+    {
+        Write-Output "ERROR: the CSV has incomplete details for the following Application: '$($CSVApp.SourceAppName)'. It has NOT been migrated."
+        continue;
+    }
+
+    Write-Output "Processing $($CSVApp.SourceAppName)"
+
+    $AppInfoExportPath = $null
+    $AppContentExportPath = $null
+
+    $AppInfoExportPath = Join-Path -Path $RootAppInfoExportPath -ChildPath $CSVApp.SourceAppName
+    $AppContentExportPath = Join-Path -Path $RootAppContentExportPath -ChildPath ("$($CSVApp.FolderManufacturer)\$($CSVApp.FolderAppName)\$($CSVApp.FolderAppVersion)")
+
+
+    # Check if this app has already been processed by this script previously by checking for the App Info and App Content folders
+    if (Test-Path "FileSystem::$AppInfoExportPath")
+    {
+        Write-Output "ERROR: the path used to store this Application's information already exists ('$AppInfoExportPath'). This indicates this application has already been migrated, so it will be skipped."
+        continue;
+    }
+
+    if (Test-Path "FileSystem::$AppContentExportPath")
+    {
+        Write-Output "ERROR: the path used to store this Application's content already exists ('$AppContentExportPath'). This indicates this application has already been migrated, so it will be skipped."
+        continue;
+    }
+
+
+    Write-Output "Creating the App Info export folder '$AppInfoExportPath'"
+    New-Item -Path "FileSystem::$AppInfoExportPath" -ItemType Directory -Force | Out-Null
+
+
+    # Confirm the app exists in the Source SCCM
+    if (-not (AppExistsInSCCM -ApplicationName $CSVApp.SourceAppName))
+    {
+        Write-Output "ERROR: $($CSVApp.SourceAppName) couldn't be found in SCCM. Skipping this application."
+        continue;
+    }
+
+    # Check if the app has dependencies and add it to the list if it does
+    if (ApplicationHasDependencies -ApplicationName $CSVApp.SourceAppName)
+    {
+        $AppsThatHaveDependencies += $CSVApp.SourceAppName
+    }
+
+    # Export the App (no content exported)
+    Write-Output "Exporting the app meta data..."
+    $ExportPath = Join-Path -Path $AppInfoExportPath -ChildPath "$($CSVApp.SourceAppName).zip"
+    Export-CMApplication -Name $CSVApp.SourceAppName -Path $ExportPath -OmitContent -Comment "Exported by automation script on $((Get-Date).ToLocalTime())"
+    
+    if (-not (Test-Path "FileSystem::$ExportPath"))
+    {
+        Write-Output "ERROR: The export process did not throw an error, however, the file that is expected to exist after an export ('$ExportPath') does not exist. The export must have failed silently. Application not migrated."
+        continue;
+    }
+    else
+    {
+        Write-Output "Metadata export finished."
+    }
+
+
+    # Content handling
+
+    # Check if the app has content
+    if (AppHasSCCMContent -ApplicationName $CSVApp.SourceAppName)
+    {
+        # Create the content destination folder structure
+        Write-Output "Creating the App Content export folder '$AppContentExportPath'"
+        New-Item -Path "FileSystem::$AppContentExportPath" -ItemType Directory -Force | Out-Null
+
+        # Copy content 
+        ExportAppContent -ApplicationName $CSVApp.SourceAppName -DestinationPath $AppContentExportPath
+    }
+    else
+    {
+        Write-Output "Application doesn't have content."
+    }
+
+
+}
+
+
+Display-AppsWithDependencies -ApplicationNames $AppsThatHaveDependencies
+
+Stop-Transcript
---
+++ Import-SCCMApplications.ps1
@@ -0,0 +1,198 @@
+﻿$ErrorActionPreference = "Stop"
+
+
+#######################################################################
+#
+#   Step 1 - You MUST change these values to match your environment.
+#
+#######################################################################
+$SCCMSiteCode = "PR1"   # SCCM Site code you are importing TO
+$ProviderMachineName = "SCCM-01"   # Machine name of the SCCM server that has the SMS Provider service installed.
+$RootAppContentDestinationPath = "\\server\NewSource"   # The root folder where you want Application content to be migrated TO
+$RootAppInfoSourcePath = "\\server\share\AppInfo"       # Where Application (meta data) will be searched for to import
+$RootAppContentSourcePath = "\\server\share\Content"    # Where Application content will be copied from (to the $RootAppContentDestinationPath path above)
+
+#######################################################################
+#
+#   Step 2 - OPTIONAL: Change these if you desire.
+#
+#######################################################################
+$LogFilePath = "$($env:USERPROFILE)\Desktop\SCCMAppMigration-Import.log"    # Where this script will log to
+$AppsToProcessCsvPath = "$(Split-Path $MyInvocation.MyCommand.Path)\ToProcess.csv"    # The list of SCCM Applications that will be searched for and imported, as well as the import configuration.
+$ApplicationFolderInSCCM = "Application"   # Control where the imported Applications are placed inside the SCCM Console. Default is the root of 'Applications'. Example placement into a subfolder: "Application\SubFolder1\SubFolder2". Note 1: The subfolders must already exist. Note 2: The root folder is "Application", not "ApplicationS" (plural)
+$DistributionPointGroupName = ""   #Provide the DP Group name that all imported Applications should be distributed to. Leave this blank if you don't want the script to distribute content.
+
+
+Function AppExistsInSCCM($ApplicationName)
+{
+    $App = $null
+    $App = Get-CMApplication -Name $ApplicationName -Fast
+
+    if ($App -eq $null)
+    {
+        return $false
+    }
+    else
+    {
+        return $true
+    }
+}
+
+Function AppCSVDataIsValid($AppCSV)
+{
+    if ([string]::IsNullOrWhiteSpace($AppCSV.SourceAppName) -or
+        [string]::IsNullOrWhiteSpace($AppCSV.DestinationAppName) -or
+        [string]::IsNullOrWhiteSpace($AppCSV.FolderManufacturer) -or
+        [string]::IsNullOrWhiteSpace($AppCSV.FolderAppName) -or
+        [string]::IsNullOrWhiteSpace($AppCSV.FolderAppVersion) -or
+        [string]::IsNullOrWhiteSpace($AppCSV.FolderAppName)
+    )
+    {
+        return $false
+    }
+    else
+    {
+        return $true
+    }
+}
+
+Function DistributeApplication($ApplicationName)
+{
+    Start-CMContentDistribution -ApplicationName $ApplicationName -DistributionPointGroupName $DistributionPointGroupName
+}
+
+
+# Import the ConfigurationManager.psd1 module 
+if((Get-Module ConfigurationManager) -eq $null) {
+    Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1" 
+}
+
+# Connect to the site's drive if it is not already present
+if((Get-PSDrive -Name $SCCMSiteCode -PSProvider CMSite -ErrorAction SilentlyContinue) -eq $null) {
+    New-PSDrive -Name $SCCMSiteCode -PSProvider CMSite -Root $ProviderMachineName
+}
+
+# Set the current location to be the site code.
+Set-Location "$($SCCMSiteCode):\"
+
+Import-Module ActiveDirectory
+
+Start-Transcript -Path $LogFilePath -Append
+
+cd "$($env:SystemDrive)\"
+Write-Output "Reading CSV for apps to process..."
+$CSVApps = Import-Csv -Path $AppsToProcessCsvPath
+cd "$($SCCMSiteCode):\$($ApplicationFolderInSCCM)"
+
+Write-Output "Read $($CSVApps.Count) apps from CSV file"
+
+foreach ($CSVApp in $CSVApps)
+{
+    Write-Output ""
+    
+    # Validate the CSV provided for this app is complete before continuing
+    if (-not (AppCSVDataIsValid -AppCSV $CSVApp))
+    {
+        Write-Output "ERROR: the CSV has incomplete details for the following Application: '$($CSVApp.SourceAppName)'. It has NOT been migrated."
+        continue;
+    }
+
+    Write-Output "Processing $($CSVApp.SourceAppName)"
+
+    # Check if the App already exists in SCCM. If it does, skip it.
+    if (AppExistsInSCCM -ApplicationName $CSVApp.DestinationAppName)
+    {
+        Write-Output "ERROR: '$($CSVApp.DestinationAppName)' already exists in the destination SCCM. Not migrating again."
+        continue
+    }
+
+    # Check that the app export file exists
+    $AppInfoToImport = $null
+    $AppInfoToImport = Join-Path -Path $RootAppInfoSourcePath -ChildPath "$($CSVApp.SourceAppName)\$($CSVApp.SourceAppName).zip"
+
+    if (-not (Test-path "FileSystem::$AppInfoToImport"))
+    {
+        Write-Output "ERROR: The AppInfo file for $($CSVApp.SourceAppName) was expected at '$($AppInfoToImport)' but couldn't be found. Application skipped."
+        continue
+    }
+
+    #Import the app
+    Write-Output "Importing '$AppInfoToImport'"
+    Import-CMApplication -FilePath $AppInfoToImport
+    Write-Output "Import done."
+
+    if ($ApplicationFolderInSCCM -ne "Application")
+    {
+        #App is imported to root Application location. Move it to the desired spot.
+        Write-Output "Moving the Application to the right spot in the Console..."
+        Get-CMApplication -Name $CSVApp.SourceAppName -Fast | Move-CMObject -FolderPath "$($SCCMSiteCode):\$($ApplicationFolderInSCCM)"
+        Write-Output "Move done."
+    }
+
+    #rename if needed
+    if ($CSVApp.SourceAppName -ne $CSVApp.DestinationAppName)
+    {
+        Write-Output "Renaming the Application to '$($CSVApp.DestinationAppName)'..."
+        Get-CMApplication -Name $CSVApp.SourceAppName -Fast | Set-CMApplication -NewName $CSVApp.DestinationAppName
+        Write-Output "Rename done."
+    }
+    
+
+    #Check if there is content to copy
+    $ExpectedContentSourcePath = $null
+    $ExpectedContentSourcePath = Join-Path -Path $RootAppContentSourcePath -ChildPath ("$($CSVApp.FolderManufacturer)\$($CSVApp.FolderAppName)\$($CSVApp.FolderAppVersion)")
+
+    if (Test-Path -Path "FileSystem::$ExpectedContentSourcePath")
+    {
+        # Content exists for this app
+        $DestinationContentPath = $null
+        $DestinationContentPath = Join-Path -Path $RootAppContentDestinationPath -ChildPath ("$($CSVApp.FolderManufacturer)\$($CSVApp.FolderAppName)\$($CSVApp.FolderAppVersion)")
+
+        Write-Output "Creating the folders in the following path if any don't exist: '$DestinationContentPath'"
+        New-Item -Path "FileSystem::$DestinationContentPath" -ItemType Directory -Force | Out-Null
+
+        #Copy files
+        Write-Output "Copying files from '$ExpectedContentSourcePath' to '$DestinationContentPath'"
+        Copy-Item -Path "FileSystem::$ExpectedContentSourcePath\*" -Destination "FileSystem::$DestinationContentPath" -Recurse -Force
+        Write-Output "Finished copying"
+
+
+        #Update each Deployment Type's Content Location
+        $DTs = @(Get-CMApplication -Name $CSVApp.DestinationAppName | Get-CMDeploymentType)
+
+        foreach ($DT in $DTs)
+        {
+            $DTPath = $null
+            $DTPath = Join-Path -Path $DestinationContentPath -ChildPath $DT.LocalizedDisplayName
+            Write-Output "Setting Deployment Type '$($DT.LocalizedDisplayName)' content path to '$DTPath'"
+
+            if ($DT.Technology -eq "MSI")
+            {
+                Set-CMMsiDeploymentType -ApplicationName $CSVApp.DestinationAppName -DeploymentTypeName $DT.LocalizedDisplayName -ContentLocation $DTPath
+            }
+            elseif ($DT.Technology -eq "Script")
+            {
+                Set-CMScriptDeploymentType -ApplicationName $CSVApp.DestinationAppName -DeploymentTypeName $DT.LocalizedDisplayName -ContentLocation $DTPath
+            }
+            else
+            {
+                Write-Output "ERROR: One of the Deployment Types on Application '$($CSVApp.DestinationAppName)' is not 'Script' or 'MSI'. This migration script is only able to handle Script and MSI deployment types. You'll need to fix this app (migrate the content) manually"
+                continue
+            }
+        }
+
+        if (-not [string]::IsNullOrWhiteSpace($DistributionPointGroupName))
+        {
+            #Distribute content to DPs
+            Write-Output "Distributing Application content to predefined DPs..."
+            DistributeApplication -ApplicationName $CSVApp.DestinationAppName
+            Write-Output "Distribution started."
+        }
+    }
+    else
+    {
+        Write-Output "No content found to transfer for $($CSVApp.SourceAppName)"
+    }
+}
+
+Stop-Transcript
